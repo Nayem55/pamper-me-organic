21728cb6bad042ee4bcad72b97e629fd97cd0e0f
Nayem55 <nayem.sayed.web@gmail.com>
2024-12-19 18:20:58 +0600

    ..

--- src/Components/Ingredients.jsx
+++ src/Components/Ingredients.jsx
@@ -5,6 +5,7 @@ import arganOil from "../assets/icons/argan-oil.png";
 import coconutOil from "../assets/icons/coconut-oil.png";
 import jojobaOil from "../assets/icons/olive-oil.png";
 import { motion } from "framer-motion";
+import banner from "../assets/Images/ingredients-banner.jpg";
 
 const Ingredients = () => {
   const ingredients = [
@@ -56,46 +57,59 @@ const Ingredients = () => {
   };
 
   return (
-    <section className="my-28 w-[90%] sm:w-[80%] mx-auto flex flex-col-reverse sm:flex-row gap-6 items-center">
-      {/* Text and Ingredients Section */}
-      <div className="sm:w-[50%]">
-        <h2 className="text-4xl font-semibold text-left mb-6 text-[#0C2E01]">
-          Ingredients
-        </h2>
-        <img className="my-4 sm:hidden" src="https://i.ibb.co.com/FmkNRGZ/post24.jpg" alt="Pamper Me Ingredients" />
-        <div className="text-left">
-          <p className="mb-8 text-lg text-gray-700">
-            Pamper Me Organic Hair Oil is crafted with a powerful blend of:
-          </p>
-          <ul className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full [&_li]:flex [&_li]:gap-4 [&_img]:w-10 [&_img]:h-10 [&_strong]:text-lg [&_strong]:text-[#0C2E01]">
-            {ingredients.map(({ icon, title, desc }, index) => (
-              <motion.li
-                key={index}
-                variants={ingredientsAnimation}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true }}
-                custom={index}
-                className="items-center"
-              >
-                <img src={icon} alt={`${title} icon`} />
-                <div>
-                  <strong>{title}</strong>
-                  <p className="text-gray-600">{desc}</p>
-                </div>
-              </motion.li>
-            ))}
-          </ul>
+    <section
+      className=""
+      style={{
+        backgroundImage: `url(${banner})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="relative flex items-center justify-center min-h-screen bg-cover bg-center px-4 w-[90%] sm:w-[80%] py-24 mx-auto">
+        <div className="sm:w-[50%]">
+          <h2 className="text-4xl font-semibold text-left mb-6 text-[#0C2E01]">
+            Ingredients
+          </h2>
+          <img
+            className="my-4 sm:hidden"
+            src={img}
+            alt="Pamper Me Ingredients"
+          />
+          <div className="text-left">
+            <p className="mb-8 text-lg text-gray-700">
+              Pamper Me Organic Hair Oil is crafted with a powerful blend of:
+            </p>
+            <ul className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full [&_li]:flex [&_li]:gap-4 [&_img]:w-10 [&_img]:h-10 [&_strong]:text-lg [&_strong]:text-[#0C2E01]">
+              {ingredients.map(({ icon, title, desc }, index) => (
+                <motion.li
+                  key={index}
+                  variants={ingredientsAnimation}
+                  initial="hidden"
+                  whileInView="show"
+                  viewport={{ once: true }}
+                  custom={index}
+                  className="items-center"
+                >
+                  <img src={icon} alt={`${title} icon`} />
+                  <div>
+                    <strong>{title}</strong>
+                    <p className="text-gray-600">{desc}</p>
+                  </div>
+                </motion.li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
 
-      {/* Image Section */}
-      <div className="sm:w-[50%] flex justify-center">
-        <img
-          className="hidden sm:block w-full max-w-[90%] rounded-lg"
-          src="https://i.ibb.co.com/FmkNRGZ/post24.jpg"
-          alt="Pamper Me Ingredients Showcase"
-        />
+        {/* Image Section */}
+        <div className="sm:w-[50%] flex justify-center">
+          <img
+            className="hidden sm:block w-full max-w-[90%] rounded-lg"
+            src={img}
+            alt="Pamper Me Ingredients Showcase"
+          />
+        </div>
       </div>
     </section>
   );
